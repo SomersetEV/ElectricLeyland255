e4340dc669f0dcfb46f1b4cea232c654995bcb0e
Wonk6677 <bbament@gmail.com>
2024-12-05 23:20:08 +0000

    0.23

--- Dashboard/Leyland_255_Nextion_dash.ino/Leyland_255_Nextion_dash.ino.ino
+++ Dashboard/Leyland_255_Nextion_dash.ino/Leyland_255_Nextion_dash.ino.ino
@@ -4,6 +4,7 @@
 //#include <SoftwareSerial.h>
 #include <TinyGPS++.h>
 CAN_FRAME msg;
+String message;
 
 // CAN frame max data length
 #define MAX_CAN_FRAME_DATA_LEN   8
@@ -21,7 +22,11 @@ int mph;
 int celldelta; // from bms
 int rpm;
 int AuxBattVolt;
-int Batvoltraw;
+int Batvoltraw; // from bms
+int Batmax; // from bms
+int Batmin; // from bms
+
+
 
 Metro dashupdatetimer = Metro(500);
 Metro gpstimer = Metro(100);
@@ -56,27 +61,27 @@ void setup() {
   int filter;
   //standard
   for (int filter = 3; filter < 7; filter++) {
-  Can0.setRXFilter(filter, 0, 0, false);
-  Can1.setRXFilter(filter, 0, 0, false);
-  }  
+    Can0.setRXFilter(filter, 0, 0, false);
+    Can1.setRXFilter(filter, 0, 0, false);
+  }
 }
 
 
 void canSniff1() { //edit for Leaf canbus messages
-  
+
   Can0.read(msg);
 
   if (msg.id == 0x55A) // from leaf inverter
   {
-    inverT = (msg.data.bytes[2]-32)/1.8;//INVERTER TEMP
-   // Serial.println(inverT);
-    MotorT = (msg.data.bytes[1]-32)/1.8;//MOTOR TEMP
+    inverT = (msg.data.bytes[2] - 32) / 1.8; //INVERTER TEMP
+    // Serial.println(inverT);
+    MotorT = (msg.data.bytes[1] - 32) / 1.8; //MOTOR TEMP
 
   }
 
   if (msg.id == 0x1DA)  // from leaf inverter
   {
-    volt = ((uint16_t)msg.data.bytes[0] << 2) | (msg.data.bytes[1] >> 6);//((uint16_t)((msg.data.bytes[0] << 2) | msg.data.bytes[1] >> 6));//MEASURED VOLTAGE FROM LEAF INVERTER
+    //volt = ((uint16_t)msg.data.bytes[0] << 2) | (msg.data.bytes[1] >> 6);//((uint16_t)((msg.data.bytes[0] << 2) | msg.data.bytes[1] >> 6));//MEASURED VOLTAGE FROM LEAF INVERTER
     rpm = ((uint16_t)((msg.data.bytes[4] << 8) | msg.data.bytes[5]));
     //Serial.println(rpm);
   }
@@ -87,9 +92,8 @@ void canSniff1() { //edit for Leaf canbus messages
     SOC = (( msg.data.bytes[1] << 8) | msg.data.bytes[0]);
     //Serial.println("SIMPBMS");
   }
-    
-  
-  /*(
+
+
   if (msg.id == 0x356)//battery voltage from SIMP BMS
   {
 
@@ -97,7 +101,26 @@ void canSniff1() { //edit for Leaf canbus messages
     volt = Batvoltraw / 10;
     //Serial.println("SIMPBMS2");
   }
-  */
+
+  if (msg.id == 0x373) // highest cell voltage from SIMP BMS
+  {
+
+    Batmax = (( msg.data.bytes[3] << 8) | msg.data.bytes[2]);
+    Batmin = (( msg.data.bytes[1] << 8) | msg.data.bytes[0]);
+  }
+
+  if (msg.id == 0x3C3) // highest cell voltage from SIMP BMS
+  {
+
+    amps = msg.data.bytes[5] + (msg.data.bytes[4] << 8) + (msg.data.bytes[3] << 16) + (msg.data.bytes[2] << 24);
+  }
+
+
+  if (msg.id == 0x3C5) // figure out can ID from Zombie for Aux Batt volt
+  {
+
+    AuxBattVolt = msg.data.bytes[5];// figure this out
+  }
 
 }
 void dashupdate()
@@ -138,7 +161,7 @@ void dashupdate()
     Serial1.write(0xff);
     Serial1.write(0xff);
     Serial1.print("volt.val=");
-    Serial1.print(volt/2); //get battery pack voltage
+    Serial1.print(volt / 2); //get battery pack voltage
     Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
     Serial1.write(0xff);
     Serial1.write(0xff);
@@ -152,14 +175,29 @@ void dashupdate()
     Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
     Serial1.write(0xff);
     Serial1.write(0xff);
-    //Serial1.print("celldelta.val = ");
-    // Serial1.print(); // to do
-    //  Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
-    // Serial1.write(0xff);
-    // Serial1.write(0xff);
+    Serial1.print("delta.val = ");
+    Serial1.print(celldelta);//get max cell voltage
+    Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
+    Serial1.write(0xff);
+    Serial1.write(0xff);
     Serial1.print("rpm.val=");
     Serial1.print(rpm);
     // Serial1.print("\xFF\xFF\xFF");
+    Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
+    Serial1.write(0xff);
+    Serial1.write(0xff);
+    Serial1.print("highest.val = ");
+    Serial1.print(Batmax);//get max cell voltage
+    Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
+    Serial1.write(0xff);
+    Serial1.write(0xff);
+    Serial1.print("lowest.val = ");
+    Serial1.print(Batmin);//get max cell voltage
+    Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
+    Serial1.write(0xff);
+    Serial1.write(0xff);
+    Serial1.print("volt12v.val = ");
+    Serial1.print(AuxBattVolt);//get max cell voltage
     Serial1.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
     Serial1.write(0xff);
     Serial1.write(0xff);
@@ -182,23 +220,58 @@ void gpsupdate()// This sketch displays information every time a new sentence is
   }
 
 }
+
+void dashreturn()//
+{
+   kW = volt * amps;
+   celldelta = Batmax - Batmin;
+
+   
+}
+
+void buttonread()
+{
+ message = Serial1.read();
+
+ if (message[1] == 65){
+    CAN_FRAME msg1;
+    msg1.id = 0x111;
+    msg1.length = 8;
+    msg1.data.bytes[0] = 1;
+    msg1.data.bytes[1] = 0;
+    msg1.data.bytes[2] = 0;
+    msg1.data.bytes[3] = 0;
+    msg1.data.bytes[4] = 0;
+    msg1.data.bytes[5] = 0;
+    msg1.data.bytes[6] = 0x00;
+    msg1.data.bytes[7] = 0;
+    Can0.sendFrame(msg1);
+ }
+
+  
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   //Can0.events();
 
- if (Can0.available() > 0) {
+  if (Can0.available() > 0) {
     canSniff1();
 
   }
-  
 
- // if (gpstimer.check())
-//  {
- //   gpsupdate();
-//  }
+if (Serial1.available()){ //Is data coming through the serial from the Nextion?
+    buttonread();
+    } 
+
+
+  // if (gpstimer.check())
+  //  {
+  //   gpsupdate();
+  //  }
   // sending data to nextion display
   dashupdate();
-
+  dashreturn();
 
 
 }
